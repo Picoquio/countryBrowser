1d02052efbf35aa8638c74c4e5caf4f23f68eb5b
Picoquio <84530694+Picoquio@users.noreply.github.com>
2022-08-26 13:28:11 -0300

    componente input y la tabla

---
+++ src/app/paises/components/paises-tabla/paises-tabla.component.ts
@@ -0,0 +1,14 @@
+//muestra la tabla de los países
+
+import { Component, Input } from '@angular/core';
+import { Country } from '../../interfaces/paises.interface';
+
+@Component({
+  selector: 'app-paises-tabla',
+  templateUrl: './paises-tabla.component.html',
+})
+export class PaisesTablaComponent  {
+
+  @Input() paises:  Country[] = [] 
+
+}
